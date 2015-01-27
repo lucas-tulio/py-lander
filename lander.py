3e7e6040ffdcc26bf2d958524afe15cd5a2e79af
Lucas <lucasdnd@gmail.com>
2015-01-26 22:07:11 -0200

    Testing basic polygon rendering

--- lander.py
+++ lander.py
@@ -2,6 +2,10 @@ import pygame
 
 class Lander:
 
-  def __init__(self):
+  def __init__(self, surface):
+    self.surface = surface
     self.x = 10.0
     self.y = 10.0
+
+  def on_render(self):
+    pygame.draw.polygon(self.surface, (255, 255, 255), ((self.x, self.y), (self.x + 10, self.y + 20), (self.x + 30, self.y + 50)), 3)
